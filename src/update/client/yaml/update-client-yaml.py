3f390a7fae267de9dd3c5a6f775a67870985637a
Jagadeesh Malakannavar <mnjagadeesh@gmail.com>
2018-08-10 06:36:25 +0530

    Initial check it

--- src/update/client/yaml/update-client-yaml.py
+++ src/update/client/yaml/update-client-yaml.py
@@ -4,7 +4,21 @@
 # Read generic config
 # update in all client folder
 
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(module)s %(name)s.%(funcName)s +%(lineno)s: %(levelname)-8s [%(process)d] %(message)s',
+                    )
+
 class Update(object):
     def __init__(self):
+        pass
 
+class ReadConfig(object):
+    """
 
+    This class collects customization and generic yaml for given clients
+
+    """
+    def __init__(self, config_filename):
+        self.config_filename = config_filename
+        logging.info("Will Read " + self.config_filename)
